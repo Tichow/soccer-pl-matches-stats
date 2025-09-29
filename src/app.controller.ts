fcc92fc20ecde16ab57507f1ebd7534c8592ba3e
Tichow <matteo.quintaneiro@gmail.com>
2025-09-29 09:22:21 +0200

    feat: github link from root for easier understanding

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getApiInfo() {
+    return {
+      name: "Soccer PL Matches Stats API",
+      description: "API REST pour les statistiques de Premier League 2024-2025",
+      authors: ["Matteo Quintaneiro", "Maël Antunes"],
+      
+      github: "https://github.com/matteoquintaneiro/soccer-pl-matches-stats",
+      
+      quick_test: [
+        "/matches/summary - Résumé des matchs",
+        "/matches/detailed - Matchs détaillés", 
+        "/matches/stats - Statistiques générales",
+        "/matches/classement - Classement Premier League"
+      ]
+    };
+  }
+}
