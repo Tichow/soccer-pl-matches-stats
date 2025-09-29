f389c63dcef487aec855085f7b372b5218e56351
Tichow <matteo.quintaneiro@gmail.com>
2025-09-29 09:25:21 +0200

    fix: github link from root for easier understanding

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AppController {
       description: "API REST pour les statistiques de Premier League 2024-2025",
       authors: ["Matteo Quintaneiro", "Maël Antunes"],
       
-      github: "https://github.com/matteoquintaneiro/soccer-pl-matches-stats",
+      github: "https://github.com/Tichow/soccer-pl-matches-stats",
       
       quick_test: [
         "/matches/summary - Résumé des matchs",
